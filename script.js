14c434433c6b51e98ee2f7ccd3117bef002df32e
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-02-03 23:12:48 +0530

    QR not working its inder the process

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let QRtext = document.getElementById("QRtext");
 let QRimage = document.getElementById("QRimage");
 
 function GenerateQR(){
-    QRimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRtext.ariaValueMax;
+    if(QRtext.value.length > 0){
+        QRimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRtext.ariaValueMax;
     imgBox.classList.add("show-img")
+    }else{
+        QRtext.classList.add("error")
+        setTimeout(()=>{
+            QRtext.classList.add("error")
+        },1000)
+    }
 }
